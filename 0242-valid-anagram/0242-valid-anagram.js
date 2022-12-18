1a860ccea4cb2702759ddc39134a03819cfce913
Michael Ruiz <104645008+Ruizmichael@users.noreply.github.com>
2022-12-17 22:50:03 -0800

    Time: 108 ms (76.64%), Space: 43.2 MB (76.68%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -3,18 +3,20 @@
  * @param {string} t
  * @return {boolean}
  */
+
+const createCache = (str) => {
+    return str.split('').reduce((acc, cur) => {
+        acc[cur] ? acc[cur]++ : acc[cur] = 1;
+        return acc;
+    }, {});
+}
+
 const isAnagram = (s, t) => {
     if (s.length !== t.length) return false;
     
-    const cacheS = s.split('').reduce((acc, cur) => {
-        acc[cur] ? acc[cur]++ : acc[cur] = 1;
-        return acc;
-    }, {});
+    const cacheS = createCache(s)
     
-    const cacheT = t.split('').reduce((acc, cur) => {
-        acc[cur] ? acc[cur]++ : acc[cur] = 1;
-        return acc;
-    }, {});
+    const cacheT = createCache(t)
     
     for (const char in cacheT) {
         if (cacheT[char] !== cacheS[char] || !cacheS[char]) return false;
